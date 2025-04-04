75efffc88489660af54a925918d07b3692d7670c
Howaida Sayed <h.sayed@neoxero.com>
2025-04-04 18:37:38 +0200

    fetching a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,21 +2,17 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = async (req, res, next) => {
-  // send response
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post",
-        imageUrl: "images/model1.png",
-        creator: {
-          name: "Omar",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  // fetch data from database (simulated here with a console log)
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ message: "Fetched posts successfully.", posts: posts }); // send the posts data as a response
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500; // internal server error
+      }
+      next(err); // pass the error to the error handling middleware
+    });
 };
 
 exports.createPost = async (req, res, next) => {
@@ -55,6 +51,26 @@ exports.createPost = async (req, res, next) => {
         message: "Post created successfully",
         post: result,
       });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500; // internal server error
+      }
+      next(err); // pass the error to the error handling middleware
+    });
+};
+
+exports.getPost = async (req, res, next) => {
+  const postId = req.params.postId; // get the post id from the request parameters
+  // find the post by id in the database (simulated here with a console log)
+  Post.findById(postId) // find the post by id in the database
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404; // not found
+        throw error; // throw the error to be handled by the error handling middleware, so it will be caught by the catch block
+      }
+      res.status(200).json({ message: "post fetched", post: post }); // send the post data as a response
     })
     .catch((err) => {
       if (!err.statusCode) {
--- routes/feed.js
+++ routes/feed.js
@@ -2,13 +2,23 @@ const express = require("express");
 const { body } = require("express-validator");
 const router = express.Router();
 const feedController = require("../controllers/feed");
-const { getPosts, createPost } = feedController;
+const { getPosts, createPost, getPost } = feedController;
 
 // define some routes
 // /feed/posts ==> because we register the router with /feed in app.js
 router.get("/posts", getPosts);
 router.post(
   "/post",
+  [
+    body("title").trim().isLength({ min: 5 }).withMessage("Title must be at least 5 characters long."),
+    body("content").trim().isLength({ min: 5 }).withMessage("Content must be at least 5 characters long."),
+  ],
+  createPost
+);
+router.get("/post/:postId", getPost); // get a single post by id
+
+router.put(
+  "/post/:postId",
   [
     body("title").trim().isLength({ min: 5 }).withMessage("Title must be at least 5 characters long."),
     body("content").trim().isLength({ min: 5 }).withMessage("Content must be at least 5 characters long."),

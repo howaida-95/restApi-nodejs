ac449fe9fbc349b1fb379464347b31340b5747c7
Howaida Sayed <h.sayed@neoxero.com>
2025-04-23 00:03:46 +0200

    Setup and Writing a First Test

---
+++ test/start.js
@@ -0,0 +1,16 @@
+const expect = require("chai").expect; // import the expect function from chai library
+
+/*
+it is a function provided by mocha that is used to define a test case.
+it is a global function that is available in the test environment when using mocha. 
+It is used to define a test case, which is a single unit of testing that checks a specific behavior or functionality of the code being tested.
+
+-> takes 2 arguments:
+1- a string that describes the test case. important to know what the test is doing and what behavior it is checking.
+2- a function that contains the actual test code. This function is executed when the test case is run.
+*/
+it("shouldn't give the result of 6", function () {
+  const num1 = 2,
+    num2 = 3;
+  expect(num1 + num2).not.to.equal(6); // check if the sum of num1 and num2 is equal to 5
+});
